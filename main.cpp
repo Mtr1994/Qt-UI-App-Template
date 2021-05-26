b916c846a6b85f57b7e05ba188ece7b032bdee84
Mtr1994 <36178164+Mtr1994@users.noreply.github.com>
2021-05-26 11:21:46 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,17 +1,13 @@
 #include "mainwindow.h"
 
 #include <QApplication>
-#include <QFile>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
     // 加载样式
-    QFile qss(":/Resourse/qss/style.qss");
-    qss.open(QFile::ReadOnly);
-    qApp->setStyleSheet(qss.readAll());
-    qss.close();
+    qApp->setStyleSheet("file:///:/Resourse/qss/style.qss");
 
     MainWindow w;
     w.show();
