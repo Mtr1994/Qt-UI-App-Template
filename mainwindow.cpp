3e6cb6e03aa6bcac8f4469ac4049397aedfac897
Mtr1994 <36178164+Mtr1994@users.noreply.github.com>
2021-07-15 11:03:08 +0800

    Update mainwindow.cpp

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -28,8 +28,8 @@ void MainWindow::init()
     if (nullptr != screen)
     {
         QRect rect = screen->availableGeometry();
-        width = rect.width() * 0.1 < 1024 ? 1024 : rect.width() * 0.1;
-        height = rect.width() * 0.1 < 640 ? 640 : rect.width() * 0.1;
+        width = rect.width() * 0.64 < 1024 ? 1024 : rect.width() * 0.64;
+        height = rect.height() * 0.64 < 640 ? 640 : rect.height() * 0.64;
     }
 
     resize(width, height);
